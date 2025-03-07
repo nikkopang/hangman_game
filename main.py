80a27d65f42214fc9186bbbd805c7397ec5b0562
nikkopang <57596275+nikkopang@users.noreply.github.com>
2025-03-07 20:56:48 +0100

    V1 - Hangman terminal game
    Users are able to play the hangman game in terminal by running the main file

---
+++ main.py
@@ -0,0 +1,80 @@
+import random
+import pandas as pd
+from hangman_states import get_hangman_states
+
+def get_random_word(word_length=5):
+    df = pd.read_csv("10000_Words.csv")
+    df = df[df['words'].str.len() == word_length]
+    word = random.choice(df.words.to_list())
+    return word
+
+def set_game_mode():
+    user_in = ""
+    while user_in not in ["long", "short"]:
+        print("What game mode would you like? Choose either long or short.")
+        user_in = input("Game mode: ").strip().lower()
+
+    print(f"Game mode set to '{user_in}'.")
+    return user_in
+
+def check_letters_found_in_word(word1, word2):
+    letters = set(word1) # Get only the unique letters found in word1
+    matched_letters = [letter for letter in letters if letter in word2]
+    return matched_letters
+
+def get_user_in(word_length):
+    user_in = input("Guess a word: ").lower().strip()
+
+    while len(user_in) != word_length:
+        print(f"Please enter a {word_length} letter word.")
+        user_in = input("Guess a word: ").lower().strip()
+
+    return user_in
+
+def main():
+    selected_word = get_random_word().lower() # Get random 5 letter word
+    
+    # Initialise game criterias
+    is_correct = False
+    current_word = ["_"] * len(selected_word)
+    game_mode = set_game_mode()
+
+    # Display the current board state
+    hangman = get_hangman_states(game_mode)
+    attempts = 0
+
+    game_running = True
+    while game_running:
+        board = hangman[attempts]
+        print(board) # Show hangman state
+        
+        # Allow user to guess
+        print(f"Current word: {current_word}")
+        user_in = get_user_in(len(selected_word))
+
+        # Check answer
+        if user_in == selected_word:
+            is_correct = True
+            game_running = False
+
+        # Show hints based on any matching letters 
+        matched_letters = check_letters_found_in_word(user_in, selected_word)
+
+        if len(matched_letters) > 0:
+            for idx, letter in enumerate(list(selected_word)):
+                if current_word[idx] != "_":
+                    continue
+                elif letter in matched_letters:
+                    current_word[idx] = letter
+        
+        attempts += 1
+        if attempts == len(hangman): # End the game if too many attempts made
+            game_running = False
+    
+    # Game end message
+    if is_correct:
+        print(f"CONGRATULATIONS! You guessed the correct word: {selected_word}")
+    else:
+        print(f"OH NO! The word was {selected_word}. Try again next time.")
+            
+main()
